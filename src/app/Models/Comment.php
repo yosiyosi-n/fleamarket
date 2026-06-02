610feadda21c17c2b31400895c3609477bedc194
yosiyosi <lkairo70@gmail.com>
2026-06-02 15:02:44 +0900

    feat:コメント送信機能、Model修正

--- src/app/Models/Comment.php
+++ src/app/Models/Comment.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    // ⬇︎ 【★ここを追加！】データベースへの保存を許可する項目を並べます ⬇︎
+    protected $fillable = [
+        'user_id',
+        'item_id',
+        'comment',
+    ];
+
+    /**
+     * 💡 ついでに：コメントしたユーザーの情報を取得するためのリレーション設定
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
